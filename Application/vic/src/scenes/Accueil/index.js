9c6012255191526042847053309fe0660bfc7495
user <jeremy.creola@gmail.com>
2019-02-08 11:51:22 +0100

    creation fichiers de base pour travail à domicile ce w-e

---
+++ Application/vic/src/scenes/Accueil/index.js
@@ -0,0 +1,8 @@
+import React, { Component, Fragment } from 'react'
+import logo from './logo.png'
+import './index.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+
+
