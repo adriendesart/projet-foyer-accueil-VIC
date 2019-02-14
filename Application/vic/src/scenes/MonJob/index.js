97ec32aed1bdc574605e1727661f3c871f1603d5
JustineCrenier <justine.crenier@gmail.com>
2019-02-14 16:23:15 +0100

    correction card job

--- Application/vic/src/scenes/MonJob/index.js
+++ Application/vic/src/scenes/MonJob/index.js
@@ -1,64 +1,40 @@
 import React, { Component, Fragment } from 'react'
 import './style.scss'
 import Jobcard  from './components/Jobcard'
-
+import TitleSection from '../../components/TitleSection'
+import FiltreForm from '../../components/FiltreForm'
+import Pagination from '../../components/Pagination'
+ 
 
 class MonJob extends Component {
     render() {
         return (
-            <Fragment>
+            <div className="row section-job">
+                <div className="col-md-11 offset-md-1">
+                    <TitleSection title="Mon job"/>
+                </div>
+   
+                <div className="col-md-8 offset-md-1">
+                    <div className="row d-flex justify-content-between">
+                        <Jobcard/>
+                        <Jobcard/>
+                        <Jobcard/>
+                        <Jobcard/>
+                        <Jobcard/>
+                        <Jobcard/>
+                        <Jobcard/>
+                        <Jobcard/>
+                        <Jobcard/>
+                    </div>
+                </div>
 
-            <div className="row">
-                <div className="col-md-8">
-                    <h3>Mon Job</h3>
+                <div className="col-md-3 col-filter">
+                    <FiltreForm />
+                </div>
+                <div className="col-md-11 offset-md-1">
+                    <Pagination />
                 </div>
             </div>
-
-            <div className="container">    
-                <div className="row">
-                    <div className="col-md-8 d-flex flex-wrap justify-content-around">
-                        <div className="separateur"><Jobcard/></div>
-                        <div className="separateur"><Jobcard/></div>
-                        <div className="separateur"><Jobcard/></div>
-
-                        <div className="separateur"><Jobcard/></div>
-                        <div className="separateur"><Jobcard/></div>
-                        <div className="separateur"><Jobcard/></div>
-
-                        <div className="separateur"><Jobcard/></div>
-                        <div className="separateur"><Jobcard/></div>
-                        <div className="separateur"><Jobcard/></div>
-                    </div>{/* FIN 1ère partie de la row*/}
-                   
-                    <div className="col-md-3 filter-jobs">
-                        <h3 className="">Filtrer</h3>
-                        
-                        <form>
-                            <input type="text" placeholder="Recherche par mots-clés"></input>
-
-                            <select name="Type de contrat">
-                                <option selected disabled>Type de contrat</option>
-                                <option value="CDI">Contrat à durée indeterminée</option>
-                                <option value="CDD">Contrat à durée déterminée</option>
-                                <option value="Interim">Intérimaire</option>
-                                <option value="PFI">PFI</option>
-                            </select> 
-
-                            <select name="Lieu">
-                                <option selected disabled>Lieu</option>
-                                <option value="Flandre">Flandre</option>
-                                <option value="Hainaut">Hainaut</option>
-                                <option value="Bruxelles">Bruxelles</option>
-                                <option value="Luxembourg">Luxembourg</option>
-                            </select> 
-
-                        </form>
-                    </div>{/* FIN de la row*/}
-                </div>
-            
-            </div>    
-
-            </Fragment>
         );
     }
 }
