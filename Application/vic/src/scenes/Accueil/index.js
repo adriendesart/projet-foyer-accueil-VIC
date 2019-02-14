876cefb094edf0dae34634e11020f136d30a8082
user <jeremy.creola@gmail.com>
2019-02-14 09:46:54 +0100

    Maj page d'accueil

--- Application/vic/src/scenes/Accueil/index.js
+++ Application/vic/src/scenes/Accueil/index.js
@@ -8,30 +8,33 @@ class Accueil extends Component {
     render() {
         return (
             <Fragment>
-                <div className = "row"> {/*Div globale du logo, du titre et du descriptif */}
-                <div className="col-md-5 offset-md-1">
-                <img className="logoVic" src={logo} alt="" srcset="" /></div> {/* Logo Vic */}
-                <div className="col-md-4 titreVic"><h1>Société royale protectrice de l'enfance</h1>
-                <p className="text-justify descriptionVic">La société royale protectrice de l'enfance collabore avec quatre associations qui prennent en charge l'aspect pédagogique par une action de prévention ou encore par l'hébergement de jeunes au sein de leurs établissements. Ces services sont agréés par la Communauté Française Wallonie Bruxelles. </p></div>
+                <div className="row"> {/*Div globale du logo, du titre et du descriptif */}
+                    <div className="col-md-5 offset-md-1">
+                        <img className="logoVic" src={logo} alt="" srcset="" /></div> {/* Logo Vic */}
+                    <div className="col-md-4 titreVic"><h1>Société royale protectrice de l'enfance</h1>
+                        <p className="text-justify descriptionVic">La société royale protectrice de l'enfance collabore avec quatre associations qui prennent en charge l'aspect pédagogique par une action de prévention ou encore par l'hébergement de jeunes au sein de leurs établissements. Ces services sont agréés par la Communauté Française Wallonie Bruxelles. </p></div>
                 </div>
 
                 <hr color="black"></hr> {/*Trait horizontal sous le logo et la description */}
 
-                <div class="container-fluid">
-                <div class="row">
-                <div className="col-md-8 offset-md-1 paEvent"> {/* pa = Page d'Accueil */}
-                <p>Evènements</p>
-                </div>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-8 offset-md-1 paEventTitle"> {/* pa = Page d'Accueil */}
+                            <p>Evènements</p>
+                        </div>
+                    </div>
                 </div>
 
-                {/* <div class="col-sm">
-                One of three columns
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-2 offset-md-1 paEvent"> {/* A appeler 3 fois quand il sera devenu un component */}
+                            <p>test</p>
+                        </div>
+                    </div>
+
+
                 </div>
-                <div class="col-sm">
-                One of three columns
-                </div>*/}
-        </div> 
-            
+
 
             </Fragment>
         );
