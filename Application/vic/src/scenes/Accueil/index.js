a93474a4c5480c6d6928b0fb15c60f32229604fa
JustineCrenier <justine.crenier@gmail.com>
2019-02-15 14:30:48 +0100

    debut correction accueil

--- Application/vic/src/scenes/Accueil/index.js
+++ Application/vic/src/scenes/Accueil/index.js
@@ -11,42 +11,36 @@ class Accueil extends Component {
             <Fragment>
                 <div className="row"> {/*Div globale du logo, du titre et du descriptif */}
                     <div className="col-md-5 offset-md-1">
-                        <img className="logoVic" src={logo} alt="" srcset="" /></div> {/* Logo Vic */}
-                    <div className="col-md-4 titreVic"><h1>Société royale protectrice de l'enfance</h1>
-                        <p className="text-justify descriptionVic">La société royale protectrice de l'enfance collabore avec quatre associations qui prennent en charge l'aspect pédagogique par une action de prévention ou encore par l'hébergement de jeunes au sein de leurs établissements. Ces services sont agréés par la Communauté Française Wallonie Bruxelles. </p></div>
-                </div>
-
-                <hr color="black"></hr> {/*Trait horizontal sous le logo et la description */}
-
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-md-8 offset-md-1 paEventTitle"> {/* pa = Page d'Accueil */}
-                            <p>Evènements</p>
-                        </div>
+                        <img className="logoVic" src={logo} alt="" srcset="" />
+                    </div> {/* Logo Vic */}
+                    <div className="col-md-4 titreVic">
+                        <h1>Société royale protectrice de l'enfance</h1>
+                        <p className="text-justify descriptionVic">La société royale protectrice de l'enfance collabore avec quatre associations qui prennent en charge l'aspect pédagogique par une action de prévention ou encore par l'hébergement de jeunes au sein de leurs établissements. Ces services sont agréés par la Communauté Française Wallonie Bruxelles. </p>
                     </div>
                 </div>
-
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-md-2 offset-md-1 paEvent"> {/* A appeler 3 fois quand il sera devenu un component */}
-                            <img className="paExempleEvent" src={photoevent} alt="" srcset="" /> {/* Photo illustratrice d'un event */}
-                            <div className="paEventDate"> {/* Date de l'event */}
-                                <span>12/03</span>
+                    
+                <div className="row">
+                    <div className="col-md-10 offset-md-1 paEventTitle"> {/* pa = Page d'Accueil */}
+                        <h2>Evènements</h2>
+                    </div>
+                    <div className="col-md-10 offset-md-1">
+                        <div className="row">
+                            <div className="col-md-4">
+                                <img className="paExempleEvent" src={photoevent} alt=""/> {/* Photo illustratrice d'un event */}
+                                <div className="paEventDate"> {/* Date de l'event */}
+                                    <span>12/03</span>
+                                </div>
+                                <h3 className="col-md-12 offset-md-1 paNameEvent">Concert de Faudel</h3> {/* Nom de l'event */}
                             </div>
-                            <p className="col-md-12 offset-md-1 paNameEvent">Concert de Faudel</p> {/* Nom de l'event */}
                         </div>
-
+                    </div>
+                    {/*Bouton Afficher plus d'évènements */}
+                    <div className="col-md-10 offset-md-1">
+                        <div className="row">
+                            <button type="button">Afficher plus</button>
+                        </div>
                     </div>
                 </div>
-
-                <div className="container-fluid">
-                    <div className="row">
-                        <button type="button" class="btnMore btn-primary col-md-2 offset-md-4">Afficher plus</button> {/*Bouton Afficher plus d'évènements */}
-                    </div>
-                </div>
-
-                <hr color="black"></hr> {/*Trait horizontal sous les event */}
-
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-md-6 offset-md-1 paContactTitle">
